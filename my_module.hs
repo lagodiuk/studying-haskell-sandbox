bad4d966fd3fe93d5e01ccaba47b5f98f7ba3b06
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2012-11-30 01:50:35 +0200

    Defined functions types + function pairs

--- my_module.hs
+++ my_module.hs
@@ -1,11 +1,19 @@
-qsort list = 
-	if null list 
-	then []
-	else 
-		qsort [x | x <- tail list, x <= head list]
-		++ [head list]
-		++ qsort [x | x <- tail list, x > head list]
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (h:t) = 
+	qsort [x | x <- t, x <= h]
+	++ [h]
+	++ qsort [x | x <- t, x > h]
 
+primes :: Int -> [Integer]
 primes count =
 	take count
 	[x | x <- [2,3..], null [y | y <- [2..(div x 2)], mod x y == 0]]
+
+pairs :: [a] -> [(a,a)]
+pairs list = pairs' list []
+
+pairs' :: [a] -> [(a,a)] -> [(a,a)]
+pairs' [] acc = reverse acc
+pairs' (_:[]) acc = pairs' [] acc
+pairs' (x:y:tl) acc = pairs' (y:tl) ((x, y):acc)
